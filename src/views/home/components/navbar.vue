358b6398fec4b311e95bcdfe9b299153b5801882
DJH <875418273@qq.com>
2020-11-27 10:24:12 +0800

    vue-cli4 vue3

---
+++ src/views/home/components/navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navBody">
+    <nav class="navMenu">
+      <span>Hone</span>
+      <span>online production</span>
+      <span>resource model</span>
+      <span>personal center</span>
+      <span>connert us</span>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.navMenu {
+  span {
+    margin: 0 40px;
+  }
+}
+</style>
---
+++ src/views/home/home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <header>
+      <img src="" alt="" srcset="" />
+      <NavBar></NavBar>
+    </header>
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/navbar";
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  components: {
+    NavBar
+  }
+};
+</script>
+
+<style scoped lang="less">
+header {
+  height: 97px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 165px;
+}
+</style>
