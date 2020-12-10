73812b47fef5587d68ae652f9bab114f43d3ab93
DJH <875418273@qq.com>
2020-12-10 18:43:55 +0800

    up

--- src/components/footerBar/index.vue
+++ src/components/footerBar/index.vue
@@ -1,3 +1,127 @@
 <template>
-  <footer></footer>
+  <footer>
+    <!-- 友情链接 -->
+    <div class="links">
+      <a href="#"><i class="JS JS-facebook"></i></a>
+      <a href="#"><i class="JS JS-tuite"></i></a>
+      <a href="#"><i class="JS JS-linked"></i></a>
+      <a href="#"><i class="JS JS-instagram"></i></a>
+      <a href="#"><i class="JS JS-pinterest"></i></a>
+    </div>
+    <div class="goPage">
+      <router-link to="online" @click="touchDown" data-path="online"
+        >online production</router-link
+      >
+      <router-link to="" @click="touchDown" data-path="model"
+        >resource model</router-link
+      >
+      <router-link to="" @click="touchDown" data-path="personal"
+        >personal center</router-link
+      >
+      <router-link to="" @click="touchDown" data-path="us"
+        >connect us</router-link
+      >
+    </div>
+    <div class="language">
+      <i class="JS JS-diqiu"></i>
+      <span>English</span>
+    </div>
+    <div class="copyright">
+      <i class="JS JS-anquan"></i>
+      <span>Property in copyright2020-2020 ASYWARE.cn</span>
+    </div>
+  </footer>
 </template>
+
+<script>
+export default {
+  name: "footerBar",
+  data() {
+    return {};
+  },
+  methods: {
+    touchDown(event) {
+      let ev = event.currentTarget || event.srcElement;
+      this.$store.dispatch("nav/AsyncHeaderPath", ev.dataset.path);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+footer {
+  padding-top: 2.291667rem;
+  padding-bottom: 0.260417rem;
+  background: url("../../assets/images/home/footer/footer-bj.png") no-repeat;
+  background-size: 100% 100%;
+  > div {
+    display: flex;
+    justify-content: center;
+    > a {
+      display: block;
+    }
+  }
+}
+.links {
+  margin-bottom: 0.208333rem;
+  a {
+    margin: 0 0.078125rem;
+    i {
+      font-size: 0.208333rem;
+      color: #b4e5ed;
+    }
+  }
+}
+.goPage {
+  margin-bottom: 0.260417rem;
+  a {
+    margin: 0 0.078125rem;
+    font-size: 0.09375rem;
+    font-family: Arial;
+    color: #ffffff;
+  }
+}
+.language,
+.copyright {
+  align-items: center;
+  color: #8cd7e3;
+  margin-bottom: 0.09375rem;
+  i {
+    font-size: 0.125rem;
+    color: #8cd7e3;
+    margin-right: 0.026042rem;
+  }
+  span {
+    font-size: 0.083333rem;
+  }
+}
+.copyright {
+  margin-bottom: 0;
+}
+@media screen and (max-width: 960px) {
+  footer {
+    padding: 0.4rem 0 0.2933rem;
+    background-image: none;
+    background-color: #30b7cd;
+    .links {
+      margin-bottom: 0.2666rem;
+      i {
+        font-size: 0.5333rem;
+        margin: 0 0.2rem;
+      }
+    }
+    .goPage {
+      display: none;
+    }
+    .language,
+    .copyright {
+      i {
+        font-size: 0.333rem;
+      }
+      span {
+        font-size: 0.22666rem;
+      }
+    }
+  }
+}
+</style>
--- src/main/index.vue
+++ src/main/index.vue
@@ -3,11 +3,13 @@
   <main>
     <router-view></router-view>
   </main>
+  <footer-bar></footer-bar>
 </template>
 <script>
 import HeadNav from "@/components/NavBar/index";
+import FooterBar from "@/components/footerBar/index";
 export default {
   name: "publicPage",
-  components: { HeadNav }
+  components: { HeadNav, FooterBar }
 };
 </script>
