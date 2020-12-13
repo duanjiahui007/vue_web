11fb29c868cbc7a1ea10fb1b764799e0afc771b0
duanjiahui007 <875418273@qq.com>
2020-12-14 03:28:44 +0800

    '登录'

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,16 +3,35 @@
   <div class="login">
     <div class="login-content">
       <div class="login-data">
-        <h4></h4>
-        <p></p>
-        <a-button type="primary"> Primary</a-button>
-        <a-form layout="horizontal" :model="form">
+        <h4>{{ title }}</h4>
+        <p v-html="erTip"></p>
+        <a-form layout="vertical" :model="form" class="form-box">
           <a-form-item label="ID/Email">
-            <a-input v-model:value="form.email" />
+            <a-input
+              v-model:value="form.email"
+              placeholder="email@168.com"
+              allowClear
+            />
+          </a-form-item>
+          <a-form-item label="Password">
+            <a-input-password v-model:value="form.pwd" allowClear />
+          </a-form-item>
+          <a-form-item class="btnItem">
+            <a-button type="primary" shape="round" :loading="loading"
+              >continue</a-button
+            >
           </a-form-item>
         </a-form>
       </div>
-      <div class="login-mode"></div>
+      <div class="login-mode">
+        <div class="btnLink"><i class="JS JS--twitter"></i> Twitter</div>
+        <div class="btnLink"><i class="JS JS-facebooBig"></i>Facebook</div>
+        <div class="btnLink"><i class="JS JS-linkedin"></i>Linkedin</div>
+        <p>
+          By signing up, you agree to our terms of service, privacy policy and
+          email
+        </p>
+      </div>
     </div>
   </div>
   <footer-bar class="sign-footer"></footer-bar>
@@ -26,9 +45,12 @@ export default {
   data() {
     return {
       form: {
-        email: "dd",
+        email: "",
         pwd: ""
-      }
+      },
+      title: "Welcome to Log on",
+      erTip: `If you do not already have an account, please <a href="#/signUp">sign up</a> now `,
+      loading: false
     };
   },
   components: {
@@ -64,6 +86,80 @@ export default {
     display: flex;
     > div {
       flex-basis: 50%;
+      margin-bottom: 0.416667rem;
+    }
+    .login-data {
+      h4 {
+        font-size: 0.166667rem;
+        line-height: 0.4375rem;
+        color: #172945;
+        font-family: "Georgia";
+      }
+      p {
+        font-size: 0.072917rem;
+        color: #7c889c;
+        font-family: "Arial";
+        margin-bottom: 0.177083rem;
+        a {
+          color: #08cdee;
+        }
+      }
+      .form-box {
+        padding: 0 0.9375rem 0 0.25rem;
+        box-sizing: border-box;
+        .ant-form-item {
+          .ant-form-item-control {
+            .ant-input-affix-wrapper {
+              background-color: #eff1f1;
+              line-height: 0.260417rem;
+              /deep/.ant-input {
+                background-color: #eff1f1;
+                font-size: 16px;
+              }
+            }
+          }
+        }
+        .btnItem {
+          margin-bottom: 0;
+          /deep/.ant-form-item-control {
+            text-align: center;
+          }
+        }
+      }
+    }
+    .login-mode {
+      padding: 0 0.416667rem;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      background-image: url("../../assets/images/sign/bj.png");
+      background-position: right bottom;
+      background-size: 100%;
+      background-repeat: no-repeat;
+      .btnLink {
+        border-radius: 50px;
+        border: 1px solid #83bfc9;
+        width: 0.9375rem;
+        height: 0.25rem;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        padding-left: 7%;
+        margin-top: 0.208333rem;
+        i {
+          font-size: 20px;
+          margin-right: 0.052083rem;
+          color: #3c5a99;
+        }
+      }
+      p {
+        font-size: 0.083333rem;
+        line-height: 0.125rem;
+        color: #b5babf;
+        font-family: "Arial";
+        padding-right: 0.416667rem;
+        margin-top: 0.208333rem;
+      }
     }
   }
 }
